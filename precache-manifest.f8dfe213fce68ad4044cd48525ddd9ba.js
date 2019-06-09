551a1a62fdcce32b4da2f23b5c6f2eeccc6fe93f
happyvirus_sangyong <happyvirus_sangyong@naver.com>
2019-06-09 22:08:08 +0900

    Updates

---
+++ precache-manifest.f8dfe213fce68ad4044cd48525ddd9ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a226f432167c1f8b82c58a2e2888c41c",
+    "url": "/happyvirus_sangyong/index.html"
+  },
+  {
+    "revision": "0846762321ae5f373fc8",
+    "url": "/happyvirus_sangyong/static/css/main.38e490f9.chunk.css"
+  },
+  {
+    "revision": "2195b035d16517aa2b83",
+    "url": "/happyvirus_sangyong/static/js/2.77e7a1e5.chunk.js"
+  },
+  {
+    "revision": "0846762321ae5f373fc8",
+    "url": "/happyvirus_sangyong/static/js/main.82f6ec3a.chunk.js"
+  },
+  {
+    "revision": "9d64b5dc23d8c9987caa",
+    "url": "/happyvirus_sangyong/static/js/runtime~main.df046dc4.js"
+  }
+]);
